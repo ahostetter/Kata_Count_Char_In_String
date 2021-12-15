e46383ccd7b6c868b24b633240ac1c166a8c08ec
Adam Hostetter <achossy34@hotmail.com>
2021-12-14 20:05:03 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CountCharacterInString
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(Count("AdamAAASSDFSSDFAslkdfoaasdflsdf"));
+        }
+
+        static string Count(string str)
+        {
+            var dict = new Dictionary<char, int>();
+
+            foreach (char letter in str.ToLower())
+            {
+                if (!dict.ContainsKey(letter))
+                {
+                    dict.Add(letter, 1);
+                }
+                else
+                    dict[letter] = dict[letter] + 1;
+            }
+
+            string output = "";
+
+            foreach (KeyValuePair<char, int> pair in dict)
+            {
+                output += string.Format("'{0}': {1}, ", pair.Key, pair.Value);
+            }
+            return output;
+        }
+    }
+}
